ca2d189916874d1385a733079e25aa6d6a3e5503
kim dawoon <dwkim1@greenlabs.co.kr>
2022-02-13 12:26:27 +0900

    모듈 예시

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import { createStore } from "vuex";
+import autho from "./modules/autho"
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    autho
+  },
 });
---
+++ src/store/modules/autho.js
@@ -0,0 +1,9 @@
+export const autho = {
+    state: {
+        name: 'dawoon',
+        userId: 'dawoon6487'
+    },
+    mutations: {},
+    actions: {},
+    modules: {},
+};
